197a2684bb057fafd84f31dbb39782557b5d6e51
Roger <rwm810@gmail.com>
2025-02-08 21:45:47 -0500

    psn status

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.MESSAGE_CONTENT] });
+const { Client } = require('discord.js');
+const client = new Client();
 
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('messageCreate', message => {
-    if (message.content === '!ping') {
-        message.channel.send('Pong!');
-    }
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN);

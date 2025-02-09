5acae3115a830ece3329b031cd32c36f748f636d
Roger <rwm810@gmail.com>
2025-02-08 21:49:23 -0500

    update index

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const { Client } = require('discord.js');
-const client = new Client();
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
+// No message handler needed since this bot only updates channel color
 client.login(process.env.DISCORD_BOT_TOKEN);

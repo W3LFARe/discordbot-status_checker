a19dafbb299ae705dcc6689af33e2a36a01d1ff9
Roger <rwm810@gmail.com>
2025-02-08 21:40:15 -0500

    update index

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const { Client, GatewayIntentBits } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.MESSAGE_CONTENT] });
 
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);

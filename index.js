bf1c31451e3de1369e6371748d8fcff1beafb378
Roger <rwm810@gmail.com>
2025-02-08 21:33:27 -0500

    add index and package

---
+++ index.js
@@ -0,0 +1,14 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', message => {
+    if (message.content === '!ping') {
+        message.channel.send('Pong!');
+    }
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
